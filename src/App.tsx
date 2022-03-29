65f9dffd1a71e1e8495b09d93f85a5566da567e7
vyxxr <vyctor@tuta.io>
2022-03-28 22:40:20 -0300

    sem React Query

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { useFetch } from './hook/useFetch';
+
+type Repository = {
+  full_name: string;
+  description: string;
+}
+
+function App() {
+  const { data: repositories, isFetching } = useFetch<Repository>('users/vyxxr/repos')
+
+  return (
+    <ul>
+      {isFetching && <p>Carregando...</p>}
+      {repositories?.map(repo => {
+        return (
+          <li key={repo.full_name}>
+            <strong>{repo.full_name}</strong>
+            <p>{repo.description}</p>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+export default App
